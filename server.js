dd0c620473b2b9660bc802b3b58c05e661b3f475
Dani-Alarcon <daniel.alarcon.7e6@itb.cat>
2025-10-16 15:07:42 +0200

    jueves

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ import { UserRepository } from './user-repository.js';
 import jwt from 'jsonwebtoken'
 import cookieParser from 'cookie-parser';
 
+
 const app = express();
 app.use(express.json());
 app.use(cookieParser())
